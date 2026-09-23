dfff56e9818dfc2f5b95f6a5c72ded352576467b
Lucca Xavier <124697681+Lucca-Xavier@users.noreply.github.com>
2026-09-22 21:58:53 -0300

    Initial Commit Spring Initializer

---
+++ PortoesDoCeu/src/main/java/com/example/PortoesDoCeu/PortoesDoCeuApplication.java
@@ -0,0 +1,13 @@
+package com.example.PortoesDoCeu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortoesDoCeuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortoesDoCeuApplication.class, args);
+	}
+
+}
